dd3bec586ea54f8aec598f763cbc27188ed712fc
Ral Oliver <raloliver.ws@gmail.com>
2017-06-15 06:19:49 -0300

    .get & .json

--- config/express.js
+++ config/express.js
@@ -25,5 +25,23 @@ app.get('/v1/fotos', function(req, res){
     res.json(fotos);
 });
 
+//grupos (categorias) das fotos
+app.get('/v1/grupos', function(req, res){
+    'use strict';
+
+    let grupos = [
+        {
+            _id: 1,
+            nome: 'Pessoas'            
+        },
+        {
+            _id: 2,
+            nome: 'Felinos'            
+        }
+    ]
+    // aqui ainda é possível usar o .send, mas com o .json ele envia um json vazio para a requisição via url
+    res.json(grupos);
+});
+
 //Express: conjunto de filtros e middlewares que são aplicados em cada requisição.
 module.exports = app;
