d2bcb296ec1ad0476cd65f1f5ac68a5b05e29464
Ral Oliver <raloliver.ws@gmail.com>
2017-07-02 21:23:54 -0300

    Busca por :id

--- app/api/foto.js
+++ app/api/foto.js
@@ -1,9 +1,5 @@
 var api = {};
-
-api.lista = function (req, res) {
-    'use strict';
-
-    let fotos = [{
+var fotos = [{
             _id: 1,
             titulo: 'Seu Animal',
             url: 'https://i.ytimg.com/vi/inpRW4I0Lpc/maxresdefault.jpg'
@@ -14,8 +10,21 @@ api.lista = function (req, res) {
             url: 'https://i.ytimg.com/vi/DM9cV1fF518/maxresdefault.jpg'
         }
     ]
+
+api.lista = function (req, res) {    
+    
     //método json para renderizar em formato de dados do tipo json (like an API)
     res.json(fotos);
+};
+
+api.buscaId = function (req, res) {
+    'use strict'
+    //find funciona como um foreach
+    let foto = fotos.find(function (foto) {
+        return foto._id == req.params.id;
+    });
+
+    res.json(foto);
 };
 
 module.exports = api;
--- app/routes/foto.js
+++ app/routes/foto.js
@@ -4,8 +4,15 @@ var api = require('../api/foto'); //instância da api
 module.exports = function (app) {
     'use strict';
     let api = app.api.foto; //instância da api usando o consig (basta lembrar da estrutura de diretórios)
-    
+
     //verbo get (o Express suporta todos os verbos)
     // a res do server é o 2º parâmetro da função (verbo) .get
-    app.get('/v1/fotos', api.lista)   
+    app.get('/v1/fotos', api.lista)
+
+    /*
+    app.get('/v1/fotos/:id', function (req, res) {
+        console.log('Req p/ foto ' + req.params.id)
+    })
+    */
+    app.get('/v1/fotos/:id', api.buscaId)
 }
